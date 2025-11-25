fa2b7fe9efd2c408d6aad563274a34922bbfa4c8
rizkifirdaus24 <kikifirdaus2408@gmail.com>
2025-11-26 01:00:19 +0700

    add Pruduct Detail

--- frontend-shop/src/App.jsx
+++ frontend-shop/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home'; // <--- Import Home yang asli
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail'; // <--- INI PENTING: Import file asli
 import { CartProvider } from './context/CartContext';
 
-// Placeholder halaman lain (nanti kita buat juga)
+// Placeholder untuk Cart (masih dummy, nanti kita ganti juga)
 const Cart = () => <div className="p-10 text-center">Halaman Keranjang</div>;
-const ProductDetail = () => <div className="p-10 text-center">Halaman Detail Produk</div>;
 
 function App() {
   return (
@@ -14,8 +14,9 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} /> {/* Panggil komponen Home */}
+          <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          {/* Route di bawah ini sekarang akan memanggil komponen ProductDetail yang asli */}
           <Route path="/product/:slug" element={<ProductDetail />} />
         </Routes>
       </BrowserRouter>
